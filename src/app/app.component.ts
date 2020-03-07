aa913e15bf4d15dccf50e551362d0e7536bccaeb
abmohamm <mohd.92java@gmail.com>
2020-03-07 14:01:34 -0500

    Added changes to support object representation to display data in view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import { Hero } from './hero';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>{{ title }}</h1>
+             <h2>My Favorite Hero is : {{ myHero.name }}</h2>
+             <p>Heroes: </p>
+             <ul>
+                  <li *ngFor="let hero of heroes">
+                        {{ hero.name }}
+                  </li>
+             </ul>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'displaying-data';
+  title = 'Displaying-data';
+  heroes = [  new Hero(1, 'Windstorm'),
+              new Hero(13, 'Bombasto'),
+              new Hero(15, 'Magneta'),
+              new Hero(20, 'Tornado') ];
+  myHero = this.heroes[0];
 }
